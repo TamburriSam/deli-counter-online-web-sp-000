170119a153c6a3193cc58c0d733deef855bb827f
Sam Tamburri <tamburrisam@gmail.com>
2020-03-06 19:07:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each_with_index do |person, i|
+      current_line << " #{i+1}. #{person}"
+    end
+  end
+end
+
+def take_a_number(deli, astring)
+end
